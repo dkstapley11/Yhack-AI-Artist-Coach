1760080309c64496415520d3bbd2308b62dc30b9
andrewjdarley <45615643+andrewjdarley@users.noreply.github.com>
2024-03-09 05:17:43 -0700

    Update main.py
    Imagine Sleeping

--- main.py
+++ main.py
@@ -1,11 +1,45 @@
-import pygame, math
-import sys
+import tensorflow as tf
+import tensorflow_hub as hub
+from PIL import Image
+import numpy as np
+from scipy.spatial import distance
+import sys, os
 
-# Initialize Pygame
-pygame.init()
+model_url = "https://tfhub.dev/tensorflow/efficientnet/lite0/feature-vector/2"
+IMAGE_SHAPE = (224, 224)
+
+# Wrap the TensorFlow Hub KerasLayer in a Lambda layer
+layer = tf.keras.layers.Lambda(lambda x: hub.KerasLayer(model_url)(x))
+model = tf.keras.Sequential([layer])
+model.compile()
+print("MODEL", model)
+
+'''
+$ export MODEL=efficientnet-lite0
+$ wget https://storage.googleapis.com/cloud-tpu-checkpoints/efficientnet/${MODEL}.tar.gz
+$ tar zxf ${MODEL}.tar.gz
+$ python export_model.py --model_name=$MODEL --ckpt_dir=$MODEL --data_dir=/path/to/representative_dataset/ --output_tflite=${MODEL}_quant.tflite
+'''
+
+def extract(file):
+    file = Image.open(file).resize(IMAGE_SHAPE)
+    file = file.convert('RGB')  # Convert image to RGB
+    file = np.array(file) / 255.0
+    file = np.expand_dims(file, axis=0)
+    embedding = model.predict(file)
+    # embedding = model.predict(file[np.newaxis, ...])
+    flattened_feature = embedding.flatten()
+    return flattened_feature
+
+def dc(img1, img2, metric="cosine"):
+    return distance.cdist([img1], [img2], metric)[0]
 
 # Set frames per second
 FPS = 600
+import pygame, math
+
+# Initialize Pygame
+pygame.init()
 
 # Set surface dimensions
 surface_width = 512
@@ -81,14 +115,14 @@ for flat in flattened:
 
 imgindex = 0
 
-def draw_smooth_line(surface, prev_pos, actual_pos):
+def draw_smooth_line(surface, prev_pos, actual_pos, radius=3):
     # Calculate the distance between previous and actual positions
     distance = math.sqrt((actual_pos[0] - prev_pos[0]) ** 2 + (actual_pos[1] - prev_pos[1]) ** 2)
     
     # If the distance is less than or equal to 6, just draw a circle at the actual position
     k = 3.5
     if distance <= 6 // k:
-        pygame.draw.circle(surface, (0, 0, 0), actual_pos, 3)
+        pygame.draw.circle(surface, (0, 0, 0), actual_pos, radius)
     else:
         # Calculate the number of points needed to fill the line smoothly
         num_points = math.ceil(k * (distance / 6))
@@ -98,10 +132,10 @@ def draw_smooth_line(surface, prev_pos, actual_pos):
             # Calculate intermediate positions
             x = prev_pos[0] + (actual_pos[0] - prev_pos[0]) * i / num_points
             y = prev_pos[1] + (actual_pos[1] - prev_pos[1]) * i / num_points
-            pygame.draw.circle(surface, (0, 0, 0), (int(x), int(y)), 3)
+            pygame.draw.circle(surface, (0, 0, 0), (int(x), int(y)), radius)
     
     # Draw a circle at the actual position
-    pygame.draw.circle(surface, (0, 0, 0), actual_pos, 3)
+    pygame.draw.circle(surface, (0, 0, 0), actual_pos, radius)
 
 def modRect(rect, x):
     return rect[0] + x, rect[1] + x, rect[2] - x * 2, rect[3] - x * 2
@@ -115,6 +149,8 @@ ssr = ssr[0], ssr[1], ssr[2] - 1, ssr[3] - 1
 xLines = ((ssr[0], ssr[1]), (ssr[0] + ssr[2], ssr[1] + ssr[3])), ((ssr[0] + ssr[2], ssr[1]), (ssr[0], ssr[1] + ssr[3]))
 # font14px = pygame.font.Font("freesandbold.ttf", 14)
 font24px = pygame.font.Font("Oswald-Medium.ttf", 24)
+font32px = pygame.font.Font("Oswald-Medium.ttf", 32)
+
 
 
 
@@ -127,20 +163,24 @@ class Button:
         self.w = w
         self.action = None
     
-    def update(self):
-        pygame.draw.rect(screen, self.inColor, self.rect)
-        offset = (self.rect[2] - self.text.get_width()) // 2, (self.rect[3] - self.text.get_height()) // 2
-        screen.blit(self.text, (self.rect[0] + offset[0], self.rect[1] + offset[1]))
-        pygame.draw.rect(screen, self.outColor, self.rect, self.w)
+    def update(self, black=False):
+        if black:
+            pygame.draw.rect(screen, BLACK, self.rect)
+        else:
+            pygame.draw.rect(screen, self.inColor, self.rect)
+            offset = (self.rect[2] - self.text.get_width()) // 2, (self.rect[3] - self.text.get_height()) // 2
+            screen.blit(self.text, (self.rect[0] + offset[0], self.rect[1] + offset[1]))
+            pygame.draw.rect(screen, self.outColor, self.rect, self.w)
     
     def set_action(self, action):
         self.action = action
     
     def do(self, args=None):
-        if args:
-            self.action(*args)
-        else:
-            self.action()
+        if self.action is not None:
+            if args:
+                self.action(*args)
+            else:
+                self.action()
     
     def get_pressed(self, pos):
         return inRect(self.rect, pos)
@@ -150,18 +190,72 @@ reset = Button(resetRect, (120, 120, 255), (50, 50, 255), "RESET")
 w = 100
 runRect = (surface_rect[0] + surface_rect[2] - w, surface_rect[1] + surface_rect[3] + padding, 100, 40)
 run = Button(runRect, (255, 120, 120), (255, 45, 45), "RUN")
+running = font32px.render("Processing...", True, WHITE)
+score = None
+radius = 3
 
-import subprocess
+import time
 
 def runProgram(targetClass):
     pygame.image.save(box,'output.png')
-    subprocess.call(["python", "Grade_Image.py", str(targetClass)])
+    reset.update(True)
+    run.update(True)
+    pos = surface_rect[0] + surface_rect[2] // 2 - running.get_width() / 2, surface_rect[1] + surface_rect[3] + padding
+    pygame.draw.rect(screen, BLACK, (surface_rect[0], surface_rect[1] + surface_rect[3], surface_rect[2], 100))
+    screen.blit(running, pos)
+
+    pygame.display.update()
+    time.sleep(1)
+    cwd = os.getcwd()
+    path1 = os.path.join(cwd, "output.png")
+    path1 = path1.encode('utf-8')
+    img1 = extract(path1)
+    path2 = os.path.join(cwd, "ui_images/")
+    interger = int(targetClass)
+    path2 += "0" if interger < 10 else ""
+    path2 += str(interger) + ".png"
+    path2 = path2.encode('utf-8')
+    img2 = extract(path2)
+
+    rating = dc(img1, img2)
+    try:
+        rating = rating[0]
+    except:
+        pass
+    # subprocess.call(["python", "vector_similarity.py", str(targetClass)])
+    actScore = None
+    try:
+        actScore = float(rating) * 100
+        # TODO: APPLY ACTSCORE TRANFORMATIONS
+        global score
+        score = font32px.render(f"Score: {actScore:.2f}", True, WHITE)
+    except Exception as e:
+        print(type(e))
+    return None
 
 def clearImg():
     box.fill(WHITE)
 
 reset.set_action(clearImg)
 run.set_action(runProgram)
+
+minus = Button((img_rect[0] + img_rect[2] - 140, img_rect[1] + img_rect[3] + padding, 40, 40), (160, 160, 160), (100, 100, 100), "-")
+plus = Button((img_rect[0] + img_rect[2] - 40, img_rect[1] + img_rect[3] + padding, 40, 40), (160, 160, 160), (100, 100, 100), "+")
+radiusX = img_rect[0] + img_rect[2] - 70
+def add():
+    global radius
+    radius += 1
+    if radius > 6: 
+        radius = 6
+
+def reduce():
+    global radius
+    radius -= 1
+    if radius < 1: 
+        radius = 1
+
+minus.set_action(reduce)
+plus.set_action(add)
 
 while running:
     # Event handling
@@ -171,10 +265,18 @@ while running:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                box.fill(WHITE)
-            elif event.key == pygame.K_r:
-                pygame.image.save(box,'output.png')
+            # if event.key == pygame.K_SPACE:
+            #     box.fill(WHITE)
+            # elif event.key == pygame.K_r:
+            #     pygame.image.save(box,'output.png')
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                radius += 1
+                if radius > 6:
+                    radius = 6
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                radius -= 1
+                if radius < 1:
+                    radius = 1
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
                 # If mouse button is down, set mouse_pos relative to the center of the surface
@@ -193,8 +295,14 @@ while running:
                 
                     if run.get_pressed(mouse_pos):
                         run.do([imgindex])
+
                     elif reset.get_pressed(mouse_pos):
                         reset.do()
+                    
+                    elif plus.get_pressed(mouse_pos):
+                        plus.do()
+                    elif minus.get_pressed(mouse_pos):
+                        minus.do()
 
         elif event.type == pygame.MOUSEWHEEL:
             imgindex -= event.y
@@ -214,8 +322,11 @@ while running:
                 mouse_pos = pygame.mouse.get_pos()
 
 
+    radiusTxt = font32px.render(str(radius), True, WHITE)
+
     # Fill screen with black
     screen.fill(BLACK)
+    screen.blit(radiusTxt, (radiusX - radiusTxt.get_width() // 2, img_rect[1] + img_rect[3] + padding))
 
     # Draw the small red square
     pygame.draw.rect(screen, RED, square_rect)
@@ -230,7 +341,7 @@ while running:
         if not prev_pos:
             prev_pos = tuple(actPos)
 
-        draw_smooth_line(box, prev_pos, actPos)
+        draw_smooth_line(box, prev_pos, actPos, radius)
 
         prev_pos = tuple(actPos)
 
@@ -247,6 +358,12 @@ while running:
     
     reset.update()
     run.update()
+    minus.update()
+    plus.update()
+
+    if score:
+        pos = surface_rect[0] + surface_rect[2] // 2 - score.get_width() / 2, surface_rect[1] + surface_rect[3] + padding
+        screen.blit(score, pos)
 
     # Update the display
     pygame.display.flip()
